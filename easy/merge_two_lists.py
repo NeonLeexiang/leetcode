d401c2f2553b33b0ab5be84e741b28c578da76b7
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-09-05 10:30:29 +0800

    Create merge_two_lists.py

---
+++ easy/merge_two_lists.py
@@ -0,0 +1,35 @@
+"""
+需要学习下数据结构的python实现
+题目中的类型不是list而是listNode
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        first = head
+        while l1 != None and l2 != None:
+            if l1.val > l2.val:
+                head.next = l2
+                l2 = l2.next
+            else :
+                head.next = l1
+                l1 = l1.next
+            head = head.next
+        if l1 == None:
+            head.next = l2
+        elif l2 == None:
+            head.next = l1
+        return first.next
+
